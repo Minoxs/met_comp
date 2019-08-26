f9072f989b0a7e4b07c62bba6c9561728642a3d5
Mino on SSH <gufrgs2018@gmail.com>
2019-08-26 10:22:19 -0300

    Aula de fortran - Aula 6

---
+++ fortran/erro_de_arredondamento.f90
@@ -0,0 +1,7 @@
+program le_valor
+implicit none
+real :: a
+print *, "Informe o valor de a: "
+read *, a
+print *, "Valor lido:",a
+end program le_valor
---
+++ fortran/inteiro.f90
@@ -0,0 +1,12 @@
+program inteiro
+implicit none
+integer, parameter :: dois= 2
+integer :: v2 = dois
+integer :: x
+
+print *, "Valor da constante dois:", dois
+print *, "Valor da variávelv2:",v2
+print *, "Valor de x:"
+read *, x
+print *, "Valor lido:",x
+end program inteiro
