525c6fc9156ae7797e8f9c221a69708cf40b6281
Minoxs <gufrgs2018@gmail.com>
2019-09-23 20:25:01 -0300

    Trabalho 2 pronto

--- fortran/trabalho2.f90
+++ fortran/trabalho2.f90
@@ -4,8 +4,11 @@ implicit none
 integer :: com !Variável de comando
 integer :: pri, seg, ter, qua, xaxis, yaxis !Variáveis exercício 1
 real :: input1, input2 !Variáveis de input
-character(len=3) :: ninput !Número que será digitado pelo usuário no Exercício 2
+character(len=3) :: ninput  !Número que será digitado pelo usuário no Exercício 2
+character(len=4), dimension(3) :: res_piece !Fortran é chato de lidar com strings
 character(len=12) :: result !Número romano convertido, Exercício 2
+integer :: j !Variável do "do" no exercício 2.
+
 
 print *, "1 - Pontos no Plano Cartesiano"
 print *, "2 - Coversão Decimal-Romana"
@@ -68,23 +71,105 @@ else if (input1 .LT. 0) then !Quando X < 0
 	endif
 endif
 enddo
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Como funciona: O número é colocado em uma STRING, assim, posso manipular dígito por dígito.
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !Exercício 2!
 else if (com .EQ. 2) then
+result = "ba"
+ninput = ""
 print *, "Digite um número de no máximo 3 digitos."
 read *, ninput
 
-print *, ninput(1), ninput(2), ninput(3)
+do while (ninput(3:3) .EQ. " ") !Essa parte move o número para a casa apropriada, exemplo: 12 é lido como 120
+ninput(3:3) = ninput(2:2)       !Esse "do" move para que o 1 fique na dezena, e o 2 na unidade.
+ninput(2:2) = ninput(1:1)
+ninput(1:1) = " "
+enddo
 
-
-
-
-
-
-
-
-
+do j = 1, 3
+SELECT CASE (ninput(j:j))
+case (" ")
+res_piece(j) = " "
+cycle
+case ("0")
+res_piece(j) = " "
+cycle
+case ("1")
+	if (j .EQ. 1) then
+		res_piece(1) = "c"
+	else if (j .EQ. 2) then
+		res_piece(2) = "x"
+	else if (j .EQ. 3) then
+		res_piece(3) = "i"
+	endif
+case ("2")
+	if (j .EQ. 1) then
+		res_piece(1) = "cc"
+	else if (j .EQ. 2) then
+		res_piece(2) = "xx"
+	else if (j .EQ. 3) then
+		res_piece(3) = "ii"
+	endif
+case ("3")
+	if (j .EQ. 1) then
+		res_piece(1) = "ccc"
+	else if (j .EQ. 2) then
+		res_piece(2) = "xxx"
+	else if (j .EQ. 3) then
+		res_piece(3) = "iii"
+	endif
+case ("4")
+	if (j .EQ. 1) then
+		res_piece(1) = "cd"
+	else if (j .EQ. 2) then
+		res_piece(2) = "xl"
+	else if (j .EQ. 3) then
+		res_piece(3) = "iv"
+	endif
+case ("5")
+	if (j .EQ. 1) then
+		res_piece(1) = "d"
+	else if (j .EQ. 2) then
+		res_piece(2) = "l"
+	else if (j .EQ. 3) then
+		res_piece(3) = "v"
+	endif
+case ("6")
+	if (j .EQ. 1) then
+		res_piece(1) = "dc"
+	else if (j .EQ. 2) then
+		res_piece(2) = "lx"
+	else if (j .EQ. 3) then
+		res_piece(3) = "vi"
+	endif
+case ("7")
+	if (j .EQ. 1) then
+		res_piece(1) = "dcc"
+	else if (j .EQ. 2) then
+		res_piece(2) = "lxx"
+	else if (j .EQ. 3) then
+		res_piece(3) = "vii"
+	endif
+case ("8")
+	if (j .EQ. 1) then
+		res_piece(1) = "dccc"
+	else if (j .EQ. 2) then
+		res_piece(2) = "lxxx"
+	else if (j .EQ. 3) then
+		res_piece(3) = "viii"
+	endif
+case ("9")
+	if (j .EQ. 1) then
+		res_piece(1) = "cm"
+	else if (j .EQ. 2) then
+		res_piece(2) = "xc"
+	else if (j .EQ. 3) then
+		res_piece(3) = "ix"
+	endif 
+END SELECT
+enddo
+result = trim(res_piece(1))//trim(res_piece(2))//trim(res_piece(3))
+print *, result
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 endif
 enddo
