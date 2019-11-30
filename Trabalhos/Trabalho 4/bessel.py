f4d8b99aca577fe42a757030642459bcbc3acd5c
Minoxs <gufrgs2018@gmail.com>
2019-11-30 00:27:49 -0300

    solved decimal storage problem

--- Trabalhos/Trabalho 4/bessel.py
+++ Trabalhos/Trabalho 4/bessel.py
@@ -5,7 +5,7 @@ from decimal import *
 """
 Estou usando a biblioteca decimal para ter um numero arbitrário de digitos significativos
 """
-getcontext().prec = 52
+getcontext().prec = 60
 
 def test_dif():
 	pass
@@ -17,18 +17,17 @@ def Test():
 	#50 digitos
 	#Estimativas de erros usando valores encontrados na internet para a função
 	print("Rodando Testes: ")
-	j0_2 = Decimal(0.22389077914123566805182745464994862582515448221861)
-	j0_37 = Decimal(0.010862369724899694740993821310850856019800294935816)
+	j0_2 = Decimal("0.22389077914123566805182745464994862582515448221861")
+	j0_37 = Decimal("0.010862369724899694740993821310850856019800294935816")
 	calc_j0_2 = my_j0(2)
-	#calc_j0_37 = my_j0(37)
+	calc_j0_37 = my_j0(37)
 	#print("\n"*15 + "J0(2) - J0(2) Calculado: {}".format(j0_2 - calc_j0_2))
 	#print("J0(37) - J0(37) Calculado: {} \n".format(j0_37 - calc_j0_37))
-	print(j0_2)
-	print(calc_j0_2)
+	#print(j0_2)
+	#print(calc_j0_2)
 	for i in range(len(str(j0_2))):
 		if str(j0_2)[i] != str(calc_j0_2)[i]:
 			print("Diferença na: {} casa decimal".format(i-2))
-			
 			break
 		else:
 			continue
