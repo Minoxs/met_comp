af3a5db87633a57b6f926fe88cc79656411a7b8f
Minoxs <gufrgs2018@gmail.com>
2019-12-06 11:07:48 -0300

    UI progress

--- Trabalhos/Trabalho 4/experimental_branch/window.py
+++ Trabalhos/Trabalho 4/experimental_branch/window.py
@@ -5,17 +5,51 @@ from PIL import Image, ImageTk
 
 root = Tk()
 
-oneframe = Frame(root)
-oneframe.pack()
+class Start_Menu(Frame):
 
-def do():
-	button1.destroy()
-	button2.pack()
+	def __init__(self,master=None):
+		Frame.__init__(self,master)
+		self.master = master
+		self.init_window()
+	
+	def init_window(self):
+		self.master.title("Bessel")
+		self.pack(fill=BOTH, expand=1)
+		exit_button = Button(self, text="Quit",command=exit,height=4,width=8)
+		exit_button.place(relx = 1, rely = 1, x = -2, y = -2,anchor = SE)
+		switch_button = Button(self,text="switch",command=self.reform)
+		switch_button.grid(row=1,column=0)
 
-button2 = Button(oneframe, text="quit?", command=exit)
-button1 = Button(oneframe, text="Exit",command=do)
-button1.pack()
+	def reform(self):
+		self.destroy()
+		app=entry_menu(root)
+
+class entry_menu(Frame):
+
+	def __init__(self,master=None):
+		Frame.__init__(self,master)
+		self.master = master
+		self.init_window()
+
+	def init_window(self):
+		self.pack(fill=BOTH, expand=1)
+		back = Button(self,text="go back",command=self.switch)
+		back.pack()
+
+	def switch(self):
+		self.destroy()
+		app=Start_Menu(root)
 
 
-root.geometry("800x600")
+
+
+app = Start_Menu(root)
+
+ws = root.winfo_screenwidth()
+hs = root.winfo_screenheight()
+w=400
+h=300
+
+root.geometry("{}x{}".format(w,h))
+
 root.mainloop()
