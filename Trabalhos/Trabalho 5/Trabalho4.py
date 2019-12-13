10f887a49758e45439527cef26a4a476629817a2
Minoxs <gufrgs2018@gmail.com>
2019-12-13 09:16:21 -0300

    bus progress

--- Trabalhos/Trabalho 5/Trabalho4.py
+++ Trabalhos/Trabalho 5/Trabalho4.py
@@ -8,10 +8,15 @@ import time
 pi = "3.141592653589793"
 getcontext().prec = 100
 
-def f(x):
-	return Decimal(x) ** 2
+def f(F,x):
+	return eval(F)
 
-def trapez(f,a,b,n):
+def trapez(F,a,b,n):
+	if F == "":
+		F = x
+	if a == "" or b == "" or n == "":
+		print("Preencha os campos a,b e n")
+		return 0
 	start = time.time()
 	a = Decimal(a)
 	b = Decimal(b)
@@ -24,14 +29,16 @@ def trapez(f,a,b,n):
 			gui.OneLineProgressMeter("Integrando...",m,n,"integrate_key")
 		if event == "Cancel":
 			break
-		fx = eval(f)
-		a += step
+		fx = f(F,a)
+		try:
+			a += step
+		except TypeError:
+			print("Função não suportada")
+			return 0
 		res_sum += fx
 		m += 1
 	end = time.time()
 	return [round(res_sum*step,5),end-start]
-
-
 
 gui.change_look_and_feel('Dark Blue 3')
 layout = [
